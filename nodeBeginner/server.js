3945c60e73c53b05add0ed017fdbe5db3aaaf4f4
xiechangqing <576535019@qq.com>
2018-08-14 22:17:16 +0800

    update

--- nodeBeginner/server.js
+++ nodeBeginner/server.js
@@ -1,7 +1,18 @@
 var http = require("http");
 
-http.createServer(function (req, res) {
-    res.writeHead(200, {"Content-type": "text/plain"});
-    res.write("hello amy");
-    res.end();
-}).listen(8080);
+var server = http.createServer(serverFunction);
+
+function serverFunction(request, response) {
+    console.log(request.method);
+    console.log(request.url);
+    console.log(request.headers);
+
+    response.statusCode = 200;
+    response.writeHead(200, {"Content-Type": "text/html","char-set": "utf-8"});
+    response.setHeader('Content-Type', 'text/html; charset=utf-8');
+    response.setHeader('name','aliveAmy');
+    response.write(new Date().toString());
+    response.end();
+}
+server.listen(8080, 'localhost')
+// server.listen(8080);
